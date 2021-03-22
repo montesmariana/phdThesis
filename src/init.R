420c7aef68e124652e52d9c8c10eef260c0340f9
Mariana Montes <mariana.montes@kuleuven.be>
2021-03-22 15:42:16 +0100

    start acknowledgmentes

---
+++ src/init.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(flextable)
+
+
+
+# automatically create a bib database for R packages ----
+knitr::write_bib(c(
+  .packages(), 'bookdown', 'knitr', 'rmarkdown',
+  'cluster', 'vegan', 'Rtsne'
+), 'packages.bib')
