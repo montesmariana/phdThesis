71e1145e937d43911eca3f96057c3fa04a99c97e
Mariana Montes <mariana.montes@kuleuven.be>
2021-04-30 13:53:02 +0200

    use new citation keys and add text from montes_heylen

--- src/init.R
+++ src/init.R
@@ -6,5 +6,5 @@ library(flextable)
 # automatically create a bib database for R packages ----
 knitr::write_bib(c(
   .packages(), 'bookdown', 'knitr', 'rmarkdown',
-  'cluster', 'vegan', 'Rtsne'
-), 'packages.bib')
+  'cluster', 'vegan', 'Rtsne', "dbscan"
+), 'assets/bib/packages.bib')
